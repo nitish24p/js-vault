430453f7c2b750039c4f3a6c72fd2310c68d0351
NitishPhanse <nitsums@gmail.com>
2020-10-06 01:27:10 +0530

    fixed es lint warning

--- src/components/provider/index.js
+++ src/components/provider/index.js
@@ -7,41 +7,65 @@ import {
   paragraph,
   markdown,
   ol,
-  ul
+  ul,
 } from './customComponents';
 import colors from './colors';
 const components = {
-  h1: props => <h1 style={{ color: colors.primary }} {...props} />,
-  h2: props => <h2 style={{ color: colors.primary }} {...props} />,
-  h3: props => <h3 style={{ color: colors.primary }} {...props} />,
-  h4: props => <h4 style={{ color: colors.primary }} {...props} />,
-  h5: props => <h5 style={{ color: colors.primary }} {...props} />,
-  h6: props => (
-    <h6 style={{ color: colors.primary }} {...props}>
-      {props.children}
+  h1: ({ children, ...rest }) => (
+    <h1 style={{ color: colors.primary }} {...rest}>
+      {children}{' '}
+    </h1>
+  ),
+  h2: ({ children, ...rest }) => (
+    <h2 style={{ color: colors.primary }} {...rest}>
+      {children}
+    </h2>
+  ),
+  h3: ({ children, ...rest }) => (
+    <h3 style={{ color: colors.primary }} {...rest}>
+      {children}
+    </h3>
+  ),
+  h4: ({ children, ...rest }) => (
+    <h4 style={{ color: colors.primary }} {...rest}>
+      {children}
+    </h4>
+  ),
+  h5: ({ children, ...rest }) => (
+    <h5 style={{ color: colors.primary }} {...rest}>
+      {children}
+    </h5>
+  ),
+  h6: ({ children, ...rest }) => (
+    <h6 style={{ color: colors.primary }} {...rest}>
+      {children}
     </h6>
   ),
-  a: props => <a {...props} style={anchor} />,
-  p: props => (
-    <p {...props} style={paragraph}>
-      {props.children}
+  a: ({ children, ...rest }) => (
+    <a {...rest} style={anchor}>
+      {children}
+    </a>
+  ),
+  p: ({ children, ...rest }) => (
+    <p {...rest} style={paragraph}>
+      {children}
     </p>
   ),
-  pre: props => <div {...props} style={{ overflow: 'auto' }} />,
-  blockquote: props => <blockquote {...props} style={blockquote} />,
-  ol: props => (
+  pre: (props) => <div {...props} style={{ overflow: 'auto' }} />,
+  blockquote: (props) => <blockquote {...props} style={blockquote} />,
+  ol: (props) => (
     <ol {...props} style={ol}>
       {props.children}
     </ol>
   ),
-  ul: props => (
+  ul: (props) => (
     <ul {...props} style={ul}>
       {props.children}
     </ul>
   ),
-  code: Code
+  code: Code,
 };
-export default props => (
+export default (props) => (
   <MDXProvider components={components}>
     <div {...props} style={markdown} />
   </MDXProvider>
